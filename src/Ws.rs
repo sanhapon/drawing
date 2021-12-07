3e6e7123cd2cccd7af6f9072811c42b12a6c2177
sanhapon <sthadapradit@healthcare.com>
2021-12-07 20:09:55 +0700

    refactor

--- src/Ws.rs
+++ src/Ws.rs
@@ -1,4 +1,5 @@
 use crate::{Client, Clients, Lines, Line};
+use std::collections::LinkedList;
 use futures::{FutureExt, StreamExt};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
@@ -13,13 +14,11 @@ pub async fn client_connection(ws: WebSocket, clients: Clients, lines: Lines) {
 
     let client_rcv = UnboundedReceiverStream::new(client_rcv);
 
-    tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| async {
+    tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
         if let Err(e) = result {
             println!("error sending websocket msg: {}", e);
         }
     }));
-
-    // println!("clients -->{}", clients.lock().await.len());
 
     let uuid = Uuid::new_v4().to_simple().to_string();
     let new_client = Client {
@@ -27,40 +26,35 @@ pub async fn client_connection(ws: WebSocket, clients: Clients, lines: Lines) {
         sender: Some(client_sender),
     };
 
-    //Send what we in lines to new client
-    let the_copy = lines.lock().await.clone();
-    let mut iter = the_copy.iter();
-
-    loop {
-        let line = iter.next();
-        match line {
-            Some(line) => {
-                let line_json = serde_json::to_string(line).unwrap();
-                if let Some(sender) = &new_client.sender {
-                    sender.send(Ok(Message::text(line_json))).unwrap();
-                }
-            },
-            None => break
-        }
-    }
+    let the_lines = lines.lock().await.clone();
+    read_existing_lines_for_new_client(&the_lines, &new_client).await;
 
     // Add new client into map
     clients.lock().await.insert(uuid.clone(), new_client);
 
-    // Listen for incomming line
+    // Listen for incomming message
     while let Some(result) = client_ws_rcv.next().await {
         match result {
             Ok(msg) => {
-                send_to_all(&uuid, &msg, &clients).await;
-
                 let message = match msg.to_str() {
                     Ok(v) => v,
-                    Err(_) => return,
+                    Err(e) => {
+                        println!("got error --> {:?}", e);
+                        return;
+                    }
                 };
 
-                let line: Line = serde_json::from_str(message).unwrap();
-                lines.lock().await.push_back(line);
+                println!("message ==> {} ", message);
 
+                let json_option = serde_json::from_str(message);
+
+                // Check if we can deserialize json
+                match json_option {
+                    Ok(line) => lines.lock().await.push_back(line),
+                    Err(e) => println!("error --> {}", e)
+                }
+
+                send_to_all(&uuid, &msg, &clients).await;
             },
             Err(e) => {
                 println!("error receiving message for id {}): {}", &uuid.clone(), e);
@@ -77,9 +71,27 @@ async fn send_to_all(current_uuid: &str, msg: &warp::ws::Message, clients: &Clie
     for (client_uuid, client) in &clients.lock().await.clone() {
         if current_uuid != client_uuid {
             if let Some(sender) = &client.sender {
-                println!("send to {}: {:?}", client_uuid, msg);
                 let _ = sender.send(Ok(msg.clone()));
             }
+        }
+    }
+}
+
+async fn read_existing_lines_for_new_client(lines: &LinkedList<Line>, new_client: &Client) {
+    //Send what we in lines to new client
+    let mut iter = lines.iter();
+
+    loop {
+        let line = iter.next();
+        match line {
+            Some(line) => {
+                let line_json = serde_json::to_string(line).unwrap();
+                if let Some(sender) = &new_client.sender {
+                    // See if can send successfully
+                    sender.send(Ok(Message::text(line_json))).unwrap();
+                }
+            },
+            None => break
         }
     }
 }
