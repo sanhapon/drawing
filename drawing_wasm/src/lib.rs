a2a5df2d27c42f9472ed0e3a0c3180a43f617fae
sanhapon <sthadapradit@healthcare.com>
2021-12-12 19:11:34 +0700

    use mem_buffer

--- drawing_wasm/src/lib.rs
+++ drawing_wasm/src/lib.rs
@@ -1,6 +1,42 @@
 use wasm_bindgen::prelude::*;
 
+const WASM_MEMORY_BUFFER_SIZE: usize = 2;
+
+static mut WASM_MEMORY_BUFFER: [u8; WASM_MEMORY_BUFFER_SIZE] = [0; WASM_MEMORY_BUFFER_SIZE];
+
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    return a + b;
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn console_log_from_wasm() {
+    log("hello from wasm");
+}
+
+
+#[wasm_bindgen]
+pub fn store_value_in_wasm_memory_buffer_index_zero(value: u8) {
+    unsafe {
+        WASM_MEMORY_BUFFER[0] = value;
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_wasm_memory_buffer_pointer() -> *const u8 {
+    let pointer: *const u8;
+    unsafe {
+        pointer = WASM_MEMORY_BUFFER.as_ptr();
+    }
+    return pointer;
+}
+
+#[wasm_bindgen]
+pub fn read_wasm_memory_buffer_and_return_index_one() -> u8 {
+    let value: u8;
+    unsafe {
+        value = WASM_MEMORY_BUFFER[1];
+    }
+    return value;
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
-import init from "./pkg/drawing_wasm.js";
+import wasmInit from "./pkg/drawing_wasm.js";
 
 const runWasm = async () => {
-    const wasm = await init("./pkg/drawing_wasm_bg.wasm");
+    const rustWasm = await wasmInit("./pkg/drawing_wasm_bg.wasm");
+    rustWasm.store_value_in_wasm_memory_buffer_index_zero(24);
+    let wasmMemory = new Uint8Array(rustWasm.memory.buffer);
+    let bufferPointer = rustWasm.get_wasm_memory_buffer_pointer();
+    console.log(wasmMemory[bufferPointer + 0]);
+    wasmMemory[bufferPointer + 1] = 40;
+    console.log(wasmMemory[bufferPointer + 1]);
+    console.log(rustWasm.read_wasm_memory_buffer_and_return_index_one());
+    rustWasm.console_log_from_wasm();
 
-    const result = wasm.add(1,2);
-
-    const anotherDiv = document.getElementById("anotherDiv");
-
-    anotherDiv.textContent = `Hello world ${result}`;
 }
 
 runWasm();
