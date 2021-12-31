cb6a800ea6a0ff34c3e881e29e72014934c8387f
sanhapon <sthadapradit@healthcare.com>
2021-12-31 21:09:12 +0700

    able to draw but bug

--- drawing_wasm/src/lib.rs
+++ drawing_wasm/src/lib.rs
@@ -4,11 +4,9 @@ use std::cell::Cell;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-// use web_sys::{ErrorEvent, MessageEvent, WebSocket};
 use serde_json::json;
-
 use std::f64;
-
+use web_sys::{MessageEvent, WebSocket, CanvasRenderingContext2d};
 
 #[wasm_bindgen]
 extern "C" {
@@ -17,7 +15,6 @@ extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
-
 
 #[wasm_bindgen]
 pub fn start(ws_location: &str) -> Result<(), JsValue> {
@@ -29,7 +26,7 @@ pub fn start(ws_location: &str) -> Result<(), JsValue> {
     let mut last_x = 0_f64;
     let mut last_y = 0_f64;
 
-    let ws = web_sys::WebSocket::new("ws://localhost/ws")?;
+    let ws = WebSocket::new("ws://localhost/ws")?;
 
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document
@@ -46,19 +43,27 @@ pub fn start(ws_location: &str) -> Result<(), JsValue> {
 
     {
         let mousedown_fp = Box::new({
-                let context = context.clone();  // because context and pressed variables will be move into clousure and we can't use it after this.
+                let context = context.clone();
                 let pressed = pressed.clone();
                 let ws = ws.clone();
 
                 move |event: web_sys::MouseEvent| {
                     let new_x = event.offset_x() as f64;
                     let new_y = event.offset_y() as f64;
+
                     context.begin_path();
                     context.move_to(new_x, new_y);
                     pressed.set(true);
+
+                    last_x = new_x;
+                    last_y = new_y;
+
+                    let txt = format!("mouse down -> ({},{}) - ({},{})", last_x, last_y, new_x, new_y);
+                    log(txt.as_str());
+
                     if let Some(r) = send_msg(&ws, last_x, last_y, new_x, new_y) {
-                        last_x = r.0;
-                        last_y = r.1;
+                    //     last_x = r.0;
+                    //     last_y = r.1;
                     };
                 }
         }) as Box<dyn FnMut(_)>;
@@ -83,13 +88,15 @@ pub fn start(ws_location: &str) -> Result<(), JsValue> {
                             last_x = r.0;
                             last_y = r.1;
                         };
+                        let txt = format!("mouse move -> ({},{}) - ({},{})", last_x, last_y, new_x, new_y);
+                        log(txt.as_str());
                     }
                 }
         }) as Box<dyn FnMut(_)>;
         add_listener(&canvas, "mousemove", mousemove_fp)?;
     }
     {
-        let the_box = Box::new({
+        let mouseup_fp = Box::new({
             let context = context.clone();
             let pressed = pressed.clone();
 
@@ -97,17 +104,55 @@ pub fn start(ws_location: &str) -> Result<(), JsValue> {
                 pressed.set(false);
                 context.line_to(event.offset_x() as f64, event.offset_y() as f64);
                 context.stroke();
+
+                last_x = event.offset_x() as f64;
+                last_y = event.offset_y() as f64;
+                let txt = format!("mouse up -> ({},{})", last_x, last_y);
+                log(txt.as_str());
             }
         }) as Box<dyn FnMut(_)>;
-        add_listener(&canvas, "mouseup", the_box)?;
+        add_listener(&canvas, "mouseup", mouseup_fp)?;
     }
 
+    {
+        let msg_read_fp = Box::new({
+            let context = context.clone();
+
+            move |e: MessageEvent| {
+                if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+                    // let txt.as_string().unwrap().as_str());
+                    let json_obj : serde_json::Value = serde_json::from_str(txt.as_string().unwrap().as_str()).unwrap();
+
+                    if json_obj["msg_type"].as_str().unwrap().eq("line") {
+                        let line = &json_obj["line"];
+                        let lx :f64 = line["last_x"].to_string().parse().unwrap();
+                        let ly :f64 = line["last_y"].to_string().parse().unwrap();
+                        let nx :f64 = line["new_x"].to_string().parse().unwrap();
+                        let ny :f64 = line["new_y"].to_string().parse().unwrap();
+
+                        draw(&context, lx, ly, nx, ny);
+                    }
+                }
+            }
+        }) as Box<dyn FnMut(MessageEvent)>;
+
+        let onmessage_callback = Closure::wrap(msg_read_fp);
+
+        ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+        onmessage_callback.forget();
+    }
 
     Ok(())
 }
 
-fn send_msg(ws: &web_sys::WebSocket, last_x: f64, last_y: f64, new_x: f64, new_y: f64) -> Option<(f64, f64)> {
+fn add_listener(canvas: &web_sys::HtmlCanvasElement, event_name: &str, fn_pointer: Box<dyn FnMut(web_sys::MouseEvent)>) -> Result<(), JsValue> {
+    let callback= Closure::wrap(fn_pointer);
+    canvas.add_event_listener_with_callback(event_name, callback.as_ref().unchecked_ref())?;
+    callback.forget();
+    Ok(())
+}
 
+fn send_msg(ws: &WebSocket, last_x: f64, last_y: f64, new_x: f64, new_y: f64) -> Option<(f64, f64)> {
     let msg = json!({
         "last_x": last_x,
         "last_y": last_y,
@@ -115,49 +160,24 @@ fn send_msg(ws: &web_sys::WebSocket, last_x: f64, last_y: f64, new_x: f64, new_y
         "new_y": new_y,
     });
 
-    log(msg.to_string().as_str());
-    match ws.send_with_str(msg.to_string().as_str()) {
-        Ok(_) => log("ok"),
-        Err(e) => log(e.as_string()?.as_str()),
-    };
+    ws.send_with_str(msg.to_string().as_str()).unwrap();
 
+    let txt = format!("send -> ({},{}) and ({},{})", last_x, last_y, new_x, new_y);
+    log(txt.as_str());
+
+    // match ws.send_with_str(msg.to_string().as_str()) {
+    //     Ok(_) => log("send"),
+    //     Err(e) => log(e.as_string()?.as_str()),
+    // };
 
     Some((new_x, new_y))
 }
 
+fn draw(context: &CanvasRenderingContext2d, last_x: f64, last_y: f64, new_x: f64, new_y: f64) {
 
-fn add_listener(canvas: &web_sys::HtmlCanvasElement, event_name: &str, fn_pointer: Box<dyn FnMut(web_sys::MouseEvent)>) -> Result<(), JsValue> {
-    let callback= Closure::wrap(fn_pointer);
-    canvas.add_event_listener_with_callback(event_name, callback.as_ref().unchecked_ref())?;
-    callback.forget();
-Ok(())
+    context.begin_path();
+    context.move_to(last_x, last_y);
+    context.line_to(new_x, new_y);
+    context.stroke();
+    context.close_path();
 }
-
-
-// draw() {
-//     // start
-//     context.begin_path();
-//     // Draw the outer circle.
-//     context
-//         .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-//         .unwrap();
-
-//     // Draw the mouth.
-//     context.move_to(110.0, 75.0);
-//     context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-//     // Draw the left eye.
-//     context.move_to(65.0, 65.0);
-//     context
-//         .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-//         .unwrap();
-
-//     // Draw the right eye.
-//     context.move_to(95.0, 65.0);
-//     context
-//         .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-//         .unwrap();
-
-//     context.stroke();
-//     // end
-// }
