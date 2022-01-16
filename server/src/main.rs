4ea6c626f8210446f0a7d36c422fed625f038fe5
sanhapon <sthadapradit@healthcare.com>
2022-01-16 19:55:03 +0700

    update readme and improve html file

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,7 @@ use serde::{Serialize, Deserialize};
 mod ws_handlers;
 mod ws;
 
-pub const MAX_MESSAGES: usize = 100000;
+pub const MAX_MESSAGES: usize = 20000;
 
 #[derive(Debug, Clone)]
 pub struct Client {
